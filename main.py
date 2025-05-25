0284f221f00ef4d54445451f890d742cc449918d
Wizer27 <svd2yar@gmail.com>
2025-05-25 13:11:40 +0300

    add a stats? command

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ try:
     mathes = r_mathes.json()
 except:
     print("Ошибка")
-print(mathes)
 # команды
 r_teams = requests.get("https://lksh-enter.ru/teams",headers = headers)
 teams = r_teams.json()
@@ -61,5 +60,38 @@ for i in players:
 pl = sorted(set(pl), key=lambda x: x.lower())
 
 # вывод отсортированный
-for i in pl:
-    print(i)
+#for i in pl:
+    #print(i)
+  
+while True:
+    command = input("Введите команду: ")
+    if "stats?" in command:
+        team = command.split('"')[1]
+        if team not in teams_info:
+            print("0 0 0")
+            continue
+        else:
+           #print("Каманда есть в teams_info")
+           team_id = teams_info[team]["id"]
+           wins = 0
+           loses = 0
+           goals_rz = 0
+            
+           for match in mathes:
+                if match["team1"] == team_id:
+                    #print(f"Каманда {team} - это team1 ")
+                    if match["team1_score"] > match["team2_score"]:
+                        wins += 1
+                    if match["team1_score"] < match["team2_score"]:
+                        loses += 1   
+                    goals_rz += (match["team1_score"] - match["team2_score"])       
+                if match["team2"] == team_id:
+                    #print(f"Каманда {team} - это team2 ")
+                    if match["team2_score"] > match["team1_score"]:
+                        wins += 1
+                    if match["team2_score"] < match["team1_score"]:
+                        loses += 1  
+                    goals_rz += (match["team2_score"] - match["team1_score"])     
+           formatted_diff = f"+{goals_rz}" if goals_rz > 0 else str(goals_rz)
+           print(f"{wins} {loses} {formatted_diff}")
+               
